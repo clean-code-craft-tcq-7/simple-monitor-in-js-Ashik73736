312207906e9e8b22a64df8d1d8495ee4f46b7eda
Ashik S Mohammed (BD/TOA-ETS5) <sox1cob@bosch.com>
2025-08-01 12:35:37 +0530

    new reactored

--- vitals-monitor.mjs
+++ vitals-monitor.mjs
@@ -2,47 +2,60 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function blinkPatternA(times) {
+async function blinkPattern(patternFn, times) {
   for (let i = 0; i < times; i++) {
-    process.stdout.write("\r* ");
-    await delay(1000);
-    process.stdout.write("\r *");
-    await delay(1000);
+    await patternFn();
   }
 }
 
-async function blinkPatternB(times) {
-  for (let i = 0; i < times; i++) {
-    await delay(2000);
+const patternA = async () => {
+  process.stdout.write("\r* ");
+  await delay(1000);
+  process.stdout.write("\r *");
+  await delay(1000);
+};
+
+const patternB = async () => {
+  await delay(2000);
+};
+
+const patternC = async () => {
+  await delay(1000);
+  process.stdout.write("\r *");
+  await delay(1000);
+};
+
+async function checkTemperature(temp) {
+  if (temp > 102 || temp < 95) {
+    console.log("Temperature is critical!");
+    await blinkPattern(patternA, 6);
+    return false;
   }
+  return true;
 }
 
-async function blinkPatternC(times) {
-  for (let i = 0; i < times; i++) {
-    await delay(1000);
-    process.stdout.write("\r *");
-    await delay(1000);
+async function checkPulse(pulse) {
+  if (pulse < 60 || pulse > 100) {
+    console.log("Pulse Rate is out of range!");
+    await blinkPattern(patternB, 6);
+    return false;
   }
+  return true;
+}
+
+async function checkSpo2(spo2) {
+  if (spo2 < 90) {
+    console.log("Oxygen Saturation out of range!");
+    await blinkPattern(patternC, 6);
+    return false;
+  }
+  return true;
 }
 
 export async function vitalsOk(temperature, pulseRate, spo2) {
-  if (temperature > 102 || temperature < 95) {
-    console.log("Temperature is critical!");
-    await blinkPatternA(6);
-    return false;
-  }
-
-  if (pulseRate < 60 || pulseRate > 100) {
-    console.log("Pulse Rate is out of range!");
-    await blinkPatternB(6);
-    return false;
-  }
-
-  if (spo2 < 90) {
-    console.log("Oxygen Saturation out of range!");
-    await blinkPatternC(6);
-    return false;
-  }
-
-  return true;
+  return (
+    await checkTemperature(temperature) &&
+    await checkPulse(pulseRate) &&
+    await checkSpo2(spo2)
+  );
 }
