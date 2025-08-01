5f6c8f3f98e3d8756a8d752b97121fd9696b7623
Ashik S Mohammed (BD/TOA-ETS5) <sox1cob@bosch.com>
2025-08-01 12:15:33 +0530

    minor changes

--- monitor.js
+++ monitor.js
@@ -1,43 +1,31 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function displayVitalAlert(val, msg) {
+function displayVitalAlert(msg) {
   console.log(msg);
-  for (let i = 0; i < val; i++) {
-    process.stdout.write('\r* ');
-    await sleep(1000);
-    process.stdout.write('\r *');
-    await sleep(1000);
-  }
-  console.log();
 }
 
-async function isVitalOk(name, value, minVal, maxVal) {
+function isVitalOk(name, value, minVal, maxVal) {
   if (value < minVal || value > maxVal) {
-    await displayVitalAlert(6, `${name} is out of range!`);
+    displayVitalAlert(`${name} is out of range!`);
     return false;
   }
   return true;
 }
 
-export async function checkTemperature(temperature) {
+export function checkTemperature(temperature) {
   return isVitalOk('Temperature', temperature, 95, 102);
 }
 
-export async function checkPulse(pulseRate) {
+export function checkPulse(pulseRate) {
   return isVitalOk('Pulse Rate', pulseRate, 60, 100);
 }
 
-export async function checkSpo2(spo2) {
+export function checkSpo2(spo2) {
   return isVitalOk('Oxygen Saturation', spo2, 90, Infinity);
 }
 
-export async function vitalsOk(temperature, pulseRate, spo2) {
-  const results = await Promise.all([
-    checkTemperature(temperature),
-    checkPulse(pulseRate),
-    checkSpo2(spo2),
-  ]);
-  return results.every(Boolean);
+export function vitalsOk(temperature, pulseRate, spo2) {
+  return (
+    checkTemperature(temperature) &&
+    checkPulse(pulseRate) &&
+    checkSpo2(spo2)
+  );
 }
